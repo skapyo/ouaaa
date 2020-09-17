bb93bd1fb2c2789e23818c2d1f5b857eb9575abc
Yoan Olivier <yolivier@storkcom.com>
2020-09-17 17:17:20 +0200

    Agenda accessible en connecté

--- pages/agenda.jsx
+++ pages/agenda.jsx
@@ -1,14 +1,10 @@
 import { withApollo } from 'hoc/withApollo.jsx';
 import AppLayout from 'containers/layouts/AppLayout';
-import { useSessionState } from 'context/session/session';
-import FallbackAlreadyConnected from 'containers/fallbacks/FallbackAlreadyConnected';
+
 import AgendaPageLayout from 'containers/layouts/agendaPage/AgendaPageLayout';
 
 const Agenda = () => {
 
-  const user = useSessionState()
- 
-  if (user) return <FallbackAlreadyConnected />
 
   return (
     <AppLayout>
