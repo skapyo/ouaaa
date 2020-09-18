381667a76423deadf50b850066e6b2f58d87aa5d
Yoan <olivier.yoan@gmail.com>
2020-09-18 23:34:19 +0200

    INFO PRATIQUE

--- pages/actor/[id].js
+++ pages/actor/[id].js
@@ -22,7 +22,12 @@ import { withApollo } from "hoc/withApollo.jsx"
 import {useRouter} from "next/router";
 import gql from "graphql-tag";
 import {useQuery} from "@apollo/react-hooks";
-
+import Place from '@material-ui/icons/Place';
+import Phone from '@material-ui/icons/Phone';
+import AlternateEmail from '@material-ui/icons/AlternateEmail';
+import Language from '@material-ui/icons/Language';
+import Schedule from '@material-ui/icons/Schedule';
+import Paper from "@material-ui/core/Paper";
 const useStyles = makeStyles((theme) => ({
     titleContainer: {
         marginTop : theme.spacing(2),
@@ -36,7 +41,7 @@ const useStyles = makeStyles((theme) => ({
 
     },
     cardInfo: {
-        "padding": "2em",
+        "padding": "5em",
         backgroundColor:"white",
         backgroundImage:`url('/icons/planet.svg')`,
         backgroundPosition: 'right',
@@ -59,6 +64,39 @@ const useStyles = makeStyles((theme) => ({
     cardTitleCategories:{
         "color":theme.typography.h5.color,
     },
+    infoPratiqueGrid:{
+    textAlign:"center",
+
+    },
+    infoPratiqueTitle:{
+        backgroundColor: "#2a9076",
+        color:"white",
+        width:"100%",
+        padding:"1em"
+    },
+    infoPratiqueItem:{
+
+    },
+    alignLeft:{
+        textAlign: "left",
+        padding:"1em"
+    },
+    alignRight:{
+        textAlign: "right",
+        padding:"1em"
+    },
+    item:{
+
+        border: "1px solid #2a9076",
+        borderStyle: "dashed"
+    },
+    icon:{
+        color:"#bd0b3d"
+    }
+
+
+
+
 
 }))
 
@@ -120,7 +158,7 @@ const Actor = () => {
                   >
 
                       <Grid container spacing={3} >
-                          <Grid item xs={6}c lassName={styles.threePointGrid}>
+                          <Grid item xs={8} className={styles.threePointGrid}>
                               <div  className={[styles.align]}>
                                   <Typography variant="h5"   className={styles.cardTitle}  >
                                       {data && data.actor.name}
@@ -132,10 +170,54 @@ const Actor = () => {
                               <p>{data && data.actor.description}</p>
                             </Grid>
 
-                          <Grid item xs={6} className={styles.align}>
-                              <img width={"60%"} className={styles.image}
-                                   src="./image_card.jpg"
-                              />
+                          <Grid xs={3} className={[styles.align]}>
+                              <Grid container  className={[styles.infoPratiqueGrid]}>
+                                  <Typography variant="h7"   className={[styles.infoPratiqueTitle,styles.infoPratiqueItem]}  >
+                                    INFOS PRATIQUES
+                                  </Typography>
+                                  <Grid container   className={[styles.item]} >
+                                      <Grid item xs={3} className={[styles.alignRight]}>
+                                            <Place className={[styles.icon]}/>
+                                      </Grid>
+                                      <Grid item xs={8} className={[styles.alignLeft]}>
+                                          LOCALISATION La Jarne
+                                      </Grid>
+                                  </Grid>
+                                  <Grid container className={[styles.item]} >
+                                      <Grid item xs={3} className={[styles.alignRight]}>
+                                          <Phone className={[styles.icon]}/>
+                                      </Grid>
+                                      <Grid item xs={8} className={[styles.alignLeft]}>
+                                          TELEPHONE
+                                      </Grid>
+                                  </Grid>
+                                  <Grid container className={[styles.item]} >
+                                      <Grid item xs={3} className={[styles.alignRight]}>
+                                          <AlternateEmail className={[styles.icon]}/>
+                                      </Grid>
+                                      <Grid item xs={8} className={[styles.alignLeft]}>
+                                          Email
+                                      </Grid>
+                                  </Grid>
+                                  <Grid container className={[styles.item]} >
+                                      <Grid item xs={3} className={[styles.alignRight]}>
+                                          <Language className={[styles.icon]}/>
+                                      </Grid>
+                                      <Grid item xs={8} className={[styles.alignLeft]}>
+                                          SITE INTERNET
+                                      </Grid>
+                                  </Grid>
+                                  <Grid container className={[styles.item]} >
+                                      <Grid item xs={3} className={[styles.alignRight]}>
+                                          <Schedule className={[styles.icon]}/>
+                                      </Grid>
+                                      <Grid item xs={8} className={[styles.alignLeft]}>
+                                          HORAIRE
+                                      </Grid>
+                                  </Grid>
+                              </Grid>
+
+
                           </Grid>
                       </Grid>
 
