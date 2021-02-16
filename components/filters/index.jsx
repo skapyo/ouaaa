c67a42e54811ab2fe6d410bf690cca900d1e0d97
Yoan <olivier.yoan@gmail.com>
2021-02-16 23:35:03 +0100

    Filter with tree and list from collection and entries

--- components/filters/index.jsx
+++ components/filters/index.jsx
@@ -6,7 +6,9 @@ import {
   ListItem,
   ListItemIcon,
   ListItemText,
-  Collapse
+  Collapse,
+  Typography,
+  Checkbox,
 } from '@material-ui/core';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
@@ -19,77 +21,15 @@ import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import ArrowRightIcon from '@material-ui/icons/ArrowRight';
 import StyledTreeItem from './StyledTreeItem';
 
-/* function Filters(props) {
-  const { dataCategorie, categoryChange } = props;
-
-  const [checked, setChecked] = useState([0]);
-  const newChecked = [...checked];
-
-  const handleToggle = (value, index) => () => {
-    const currentIndex = checked.indexOf(value);
-
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
-    setChecked(newChecked);
-    open[index] = !open[index];
-  };
-
-  // const [open, setOpen] = React.useState([false]);
-
-  return (
-    <Grid item xs={2}>
-      <List>
-        {typeof dataCategorie !== 'undefined' && dataCategorie.categories.map((category, index) => {
-          return (
-            <div>
-              <ListItem key={category.id} role={undefined} dense button onClick={handleToggle(0, index)}>
-                <ListItemIcon />
-                <ListItemText primary={category.label} />
-                {open[index] ? <ExpandLess /> : <ExpandMore />}
-              </ListItem>
-              {typeof category.subCategories !== 'undefined' && category.subCategories != null && category.subCategories.map((subcategory, subIndex) => {
-                return (
-                  <Collapse in={open[index]} timeout="auto" unmountOnExit>
-                    <List component="div" disablePadding>
-                      <ListItem button>
-                        <ListItemIcon>
-                          <Checkbox
-                            edge="start"
-                            tabIndex={-1}
-                            disableRipple
-                            name="categories"
-                            value={subcategory.id}
-                            onChange={categoryChange}
-                          />
-                        </ListItemIcon>
-                        <ListItemText primary={subcategory.label} />
-                      </ListItem>
-                    </List>
-                  </Collapse>
-                );
-              })}
-            </div>
-          );
-        })}
-
-      </List>
-    </Grid>
-  );
-} */
-
-
-
-
-
-
 const useStyles = makeStyles({
   root: {
     height: 264,
     flexGrow: 1,
     maxWidth: 400,
+  },
+  collectionLabel: {
+    textAlign: 'center',
+    color: '#bf083e',
   },
 });
 
@@ -97,80 +37,121 @@ function Filters(props) {
   const { categoryChange } = props;
   const classes = useStyles();
 
-  const GET_CATEGORIES = gql`
-  { categories
-    {   id,
-        label
-        icon
-        color
-        
-        subCategories {
-            id,
-            label
-            icon
-            color
-            subCategories {
-                id,
-                label
-                icon
-                color
-                subCategories {
-                    label
-                    icon
-                }
-            }
-        }
-    }
+  const GET_COLLECTIONS = gql`
+{ collections
+  {   id,
+      label,
+      multipleSelection,
+      position
+      entries {
+          id,
+          label
+          subEntries {
+              id,
+              label
+          }
+      }
   }
-  `;
-
-  const [dataCategorie, setDataCategorie] = useState({});
-  const {
-    loading: loadingCategorie,
-    error: errorCategorie
-  } = useQuery(GET_CATEGORIES, {
-    fetchPolicy: 'network-only',
-    onCompleted: data => {
-      // console.log('on completed' + data);
-      setDataCategorie(data);
+}
+`;
+  function IsTree(collection) {
+    let isTree = false;
+    if (collection.entries) {
+      collection.entries.map((entry) => {
+        if (entry.subEntries) {
+          entry.subEntries.map((subentry) => {
+            isTree = true;
+            return isTree;
+          });
+        }
+      });
     }
+    return isTree;
+  }
+  const [dataCollections, setDataCollections] = useState({});
+  const {
+    loading: loadingCollections,
+    error: errorCollections,
+  } = useQuery(GET_COLLECTIONS, {
+    fetchPolicy: 'network-only',
+    onCompleted: (data) => {
+      setDataCollections(data);
+    },
   });
-  if (loadingCategorie) return 'Loading...';
-  if (errorCategorie) return `Error! ${error.message}`;
+  if (loadingCollections) return 'Loading...';
+  if (errorCollections) return `Error! ${errorCollections.message}`;
 
-  // console.log(dataCategorie);
   return (
     <Grid item xs={2}>
-      <TreeView
-        className={classes.root}
-        defaultCollapseIcon={<ArrowDropDownIcon />}
-        defaultExpandIcon={<ArrowRightIcon />}
-        defaultEndIcon={<div style={{ width: 24 }} />}
-      >
-        {dataCategorie.categories && dataCategorie.categories.map((category) => {
-          //console.log(category);
-          return (
-            <StyledTreeItem
-              key={category.id}
-              nodeId={category.id}
-              labelText={category.label} >
-              {category.subCategories && category.subCategories.map((subcategory) => {
-                //console.log(subcategory);
+      {dataCollections.collections && dataCollections.collections.map((collection) => {
+    //    const [display, setDisplay] = useState(false);
+        return (
+          <div>
+            <Typography
+              className={classes.collectionLabel}
+       //       onClick={setDisplay(!display)}
+            >
+              {collection.label}
+            </Typography>
+            { // display && 
+            IsTree(collection) && (
+            <TreeView
+              className={classes.root}
+              defaultCollapseIcon={<ArrowDropDownIcon />}
+              defaultExpandIcon={<ArrowRightIcon />}
+              defaultEndIcon={<div style={{ width: 24 }} />}
+            >
+
+              {collection.entries && collection.entries.map((entry) => {
                 return (
                   <StyledTreeItem
-                    key={subcategory.id}
-                    nodeId={subcategory.id}
-                    labelText={subcategory.label}
-                    categoryChange={categoryChange}
-                  />
-                )
-              })
-              }
-            </StyledTreeItem>
-          )
-        })
-        }
-      </TreeView>
+                    key={entry.id}
+                    nodeId={entry.id}
+                    labelText={entry.label}
+                  >
+                    {entry.subEntries && entry.subEntries.map((subEntry) => {
+                      return (
+                        <StyledTreeItem
+                          key={subEntry.id}
+                          nodeId={subEntry.id}
+                          labelText={subEntry.label}
+                          categoryChange={categoryChange}
+                        />
+                      );
+                    })}
+                  </StyledTreeItem>
+                );
+              })}
+            </TreeView>
+            )}
+            { //display &&
+             !IsTree(collection) && (
+              <List>
+                {collection.entries && collection.entries.map((entry) => {
+                  return (
+                    <ListItem
+                      key={entry.id}
+                      role={undefined}
+                      dense
+                    >
+                      <ListItemText primary={entry.label} />
+                      <Checkbox
+                        edge="start"
+                        tabIndex={-1}
+                        disableRipple
+                        onChange={categoryChange}
+                        name="{categoryChange.id}"
+                        value={entry.id}
+                        onClick={(e) => (e.stopPropagation())}
+                      />
+                    </ListItem>
+                  );
+                })}
+              </List>
+            )}
+          </div>
+        );
+      })}
     </Grid>
   );
 }
