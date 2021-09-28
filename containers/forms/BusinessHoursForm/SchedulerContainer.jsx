59d344aef4fe1fb8ea74944824db169c5d5df91f
casdidier <cheung.didier@gmail.com>
2021-09-28 17:49:47 +0400

    clean up

--- containers/forms/BusinessHoursForm/SchedulerContainer.jsx
+++ containers/forms/BusinessHoursForm/SchedulerContainer.jsx
@@ -217,7 +217,6 @@ const SchedulerContainer = (props) => {
 
         {timeContainerList.length > 0 &&
           timeContainerList.map(({ days, hours, place, id }) => {
-            console.log('this is new ID', id);
             return (
               <div className={classes.timeContainer} key={id}>
                 <TimeContainer
