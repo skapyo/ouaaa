7c8fec80bb53770e7b99996b0f04a611a63a0646
Damien Archaimbault <damien.archaimbault@laposte.net>
2022-02-28 21:55:04 +0100

    Fix home page, partners and last articles

--- containers/layouts/Partner.jsx
+++ containers/layouts/Partner.jsx
@@ -1,36 +1,40 @@
-import { useCallback, useState } from 'react';
+import React, { useCallback } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import {
-  Grid, Link ,TextField, Typography, Button,
-} from '@material-ui/core';
+import { Grid } from '@material-ui/core';
+import { useRouter } from 'next/router';
 
 const useStyles = makeStyles((theme) => ({
-  newsletter: {
+  container: {
+    marginTop: '2em'
+  },
+  partners: {
     padding: '2em 0',
     textAlign: 'center',
+    cursor: 'pointer',
     [theme.breakpoints.down('md')]: {
       padding: '1em 0',
     },
   },
-  newsletterTitle: {
+  title: {
     textAlign: 'center',
     fontSize: '32px',
-    paddingTop: '2em',
-  },
-  align: {
-    'text-align': 'center',
   },
 }));
 
 const Partner = () => {
   const styles = useStyles();
+  const router = useRouter();
+
+  const handleClickAbout = useCallback(() => {
+    router.push('/about');
+  }, [router]);
 
   return (
-    <Link href={`/about`} color="inherit" underline="none" >
-       <Grid container justifyContent="center" spacing={4} className={styles.newsletterTitle}>
-         Ils s'engagent avec nous
-       </Grid>
-      <Grid container justifyContent="center" spacing={4} className={styles.newsletter}>
+    <Grid container className={styles.container} justifyContent="center">
+      <Grid item container justifyContent="center" className={styles.title}>
+        Ils s'engagent avec nous
+      </Grid>
+      <Grid item container direction="row" justifyContent="space-around" className={styles.partners} xs={10} sm={6} onClick={handleClickAbout}>
         <Grid item>
           <img height="50px" src="https://static.ouaaa-transition.fr/static/images/actor/32/32-AU0wmu0S9A-original.jpeg?w=1920&q=75" alt="Aunis en transition" />
         </Grid>
@@ -44,10 +48,10 @@ const Partner = () => {
           <img height="50px" src="https://www.onepercentfortheplanet.fr/wp-content/uploads/2020/06/Primary_Full_Color_Horizontal-1-scaled.png" alt="onpercentfortheplanet" />
         </Grid>
         <Grid item>
-          <img height="50px"  src ="https://www.leanature.com/wp-content/uploads/2020/11/logo-lea-nature.svg" alt="leanature" />
+          <img height="50px" src="https://www.leanature.com/wp-content/uploads/2020/11/logo-lea-nature.svg" alt="leanature" />
         </Grid>
       </Grid>
-    </Link>
+    </Grid>
   );
 };
 
